01f1644769553c854b2dd93da3ff10355cfde7dd
Versifiction <marc_charpentier@hotmail.fr>
2020-07-23 16:16:04 +0200

    users model

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: String,
+  sexe: String,
+  biography: String,
+  mobilePhone: String,
+  following: Array,
+  followers: Array,
+  retweets: Array,
+  likes: Array,
+  lists: Array,
+  banner: String,
+  profilePicture: String,
+  creationDate: {
+    type: Date,
+    default: Date.now,
+  },
+  lastConnection: Date,
+  resetPasswordToken: {
+    type: String,
+    default: null,
+  },
+  resetPasswordExpires: {
+    type: Date,
+    default: null,
+  },
+  protected: Boolean,
+  verified: Boolean,
+  connected: Boolean,
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
